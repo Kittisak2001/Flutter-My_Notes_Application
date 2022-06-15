962962a66dd270eb581d8282d89f4c0b62382794
Kittisak Phatchaiphongsa <kittisak.ph2001@gmail.com>
2022-06-16 04:55:51 +0700

    Converting our Auth Process to Bloc

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/enums/menu_action.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:mynotes/utilities/dialogs/logout_dialog.dart';
 import 'package:mynotes/views/notes/note_list_view.dart';
-
 
 class NotesView extends StatefulWidget {
   const NotesView({Key? key}) : super(key: key);
@@ -43,12 +45,10 @@ class _NotesViewState extends State<NotesView> {
                   case MenuAction.logout:
                     final shouldLogout = await showLogOutDialog(context);
                     if (shouldLogout) {
-                      await AuthService.firebase().logOut();
                       // ignore: use_build_context_synchronously
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                        loginRoute,
-                        (_) => false,
-                      );
+                      context.read<AuthBolc>().add(
+                            const AuthEventLogOut(),
+                          );
                     }
                 }
               },
@@ -74,7 +74,8 @@ class _NotesViewState extends State<NotesView> {
                   return NotesListView(
                     notes: allNotes,
                     onDeleteNote: (note) async {
-                      await _noteService.deleteNote(documentId: note.documentId);
+                      await _noteService.deleteNote(
+                          documentId: note.documentId);
                     },
                     onTap: (note) {
                       Navigator.of(context).pushNamed(
